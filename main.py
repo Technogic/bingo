d579e1a98182b2a27f39304717fccb9e713aea26
aayanagarwal05@gmail.com <49789627+aayan05@users.noreply.github.com>
2020-08-09 18:38:16 +0530

    Basic Program

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import random as ra 
 import math 
 all_nos = list()
-for i in range(101):
+for i in range(1, 101):
     all_nos.append(i)
-print(all_nos)
+#print(all_nos)
+used_nos = list()
+def ranno():
+    a = ra.randrange(all_nos)
+    used_nos.append(a)
+    all_nos.remove(a)
+    print("The next number is: ",a)
+def noleft():
+    print("A total of", range(all_nos), " numbers have not been called out. \n These are: ", all_nos)
+def nosnotleft():
+    print("A total of ", range(used_nos), " have been called out. /n These are: " used_nos)
+inp = input("What do you want to do ?")
+if inp = "Next Number":
+    ranno()
+elif inp = "Numbers not called yet":
+    nosnotleft()
+elif inp="Numbers already called yet":
+    noleft()
+
 
 
