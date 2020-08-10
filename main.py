7614a419f775aec0f1b3e07d8a9d6dd0b111beb4
aayanagarwal05@gmail.com <49789627+aayan05@users.noreply.github.com>
2020-08-10 18:31:24 +0530

    Multiple numbers

--- main.py
+++ main.py
@@ -7,24 +7,23 @@ for i in range(1, 101):
 used_nos = list()
 def ranno():
     a = ra.randrange(1,101)
-    if a not in all_nos:
+    if a in used_nos:
         ranno()
     else:
         pass
     used_nos.append(a)
-    all_nos.remove(a)
     print("The next number is: ",a)
-def noleft():
-    print("The numbers have not been called out are :"+ all_nos)
 def nosnotleft():
     print("The numbers that have been called out are: ", used_nos)
 inp = input("What do you want to do ? ")
 if inp == "Next Number":
-    print(ranno())
+    inp2 = int(input("How many numbers you want ?"))
+    i = 0 
+    for i in range(inp2):
+        print(ranno())
+        i += 1
 elif inp == "Numbers not called yet":
     print(nosnotleft())
-elif inp=="Numbers already called yet":
-    print(noleft())
 else:
     print("Invalid Command")
 
