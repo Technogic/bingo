4114f6e3c2ecf74a411bff664d0df0b11be752e9
aayanagarwal05@gmail.com <49789627+aayan05@users.noreply.github.com>
2020-08-11 10:27:19 +0530

    Numbers not called yet

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import random as ra 
 import math 
 all_nos = list()
-'''
 for i in range(1, 101):
     all_nos.append(i)
 print(all_nos)
-'''
 used_nos = list()
 def ranno():
     a = ra.randrange(1,101)
@@ -14,9 +12,12 @@ def ranno():
     else:
         pass
     used_nos.append(a)
+    all_nos.remove(a)
     print("The next number is: ",a)
 def nosnotleft():
     print("The numbers that have been called out are: ", used_nos)
+def noleft():
+    print(all_nos)
 for i in range(10):
     inp = input("What do you want to do ? ")
     if inp == "Next Number":
@@ -25,15 +26,12 @@ for i in range(10):
         for i in range(inp2):
             print(ranno())
             i += 1
-    elif inp == "Numbers already called ":
+    elif inp == "Numbers already called":
         print(nosnotleft())
+    elif inp == "Numbers not called yet":
+        print(noleft())
     elif inp == "Quit":
         break
     else:
         print("Invalid Command")
     
-    
-
-
-
-
